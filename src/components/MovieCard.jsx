25829f3247e96f7e80f6b59a1d65b5b31fe4dec8
adritarahman <adrita.adi141@gmail.com>
2025-09-30 15:19:45 +0600

    fixed issues

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const MovieCard = ({movie}) => {
   return (
     <div className='movie-card'>
-       <img src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : '/no-movie.png'} alt={movie.title} />
+       <img src={movie.poster_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : '/No-Poster.png'} alt={movie.title} />
 
        <div className='mt-4'>
         <h3>{movie.title}</h3>
